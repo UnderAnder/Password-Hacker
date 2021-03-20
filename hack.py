5cb416fa31311d3da465e2d9eca1e7bfbfc3c28b
UnderAnder <andryushin.alexander@gmail.com>
2021-03-20 17:00:38 +0700

    Stage 2/5: Simple brute force

--- hack.py
+++ hack.py
@@ -1,30 +1,40 @@
 import argparse
-import socket
+from socket import socket
+from itertools import product
+
+
+class Socket:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = int(port)
+
+    def brute_pass(self):
+        with socket() as sock:
+            sock.connect((self.host, self.port))
+            for password in generate_password():
+                msg = ''.join(map(str, password)).encode()
+                sock.send(msg)
+                resp = sock.recv(1024).decode()
+                if resp == 'Connection success!':
+                    print(msg.decode())
+                    break
+
+
+def generate_password():
+    index = 1
+    while True:
+        abc = 'abcdefghijklmnopqrstuvwxyz1234567890'
+        yield from product(abc, repeat=index)
+        index += 1
 
 
 def main():
     parse_args = argparse.ArgumentParser()
-    parse_args.add_argument("address")
-    parse_args.add_argument("port")
-    parse_args.add_argument("message")
+    parse_args.add_argument('address')
+    parse_args.add_argument('port')
     args = parse_args.parse_args()
 
-    with socket.socket() as client_socket:
-        hostname = args.address
-        port = int(args.port)
-        address = (hostname, port)
-
-        client_socket.connect(address)
-
-        data = args.message
-        data = data.encode()
-
-        client_socket.send(data)
-
-        response = client_socket.recv(1024)
-
-        response = response.decode()
-        print(response)
+    Socket(args.address, args.port).brute_pass()
 
 
 if __name__ == '__main__':
